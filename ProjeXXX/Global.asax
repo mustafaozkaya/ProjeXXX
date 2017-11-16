6be42244139e5362d19efda3baf78aa2a3e7524f
Mustafa Özkaya <mustafaozkaya48@gmail.com>
2017-11-16 23:05:49 +0200

    Değişim1
    Mustafa Özkaya

---
+++ ProjeXXX/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjeXXX.MvcApplication" Language="C#" %>
---
+++ ProjeXXX/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjeXXX.Startup))]
+namespace ProjeXXX
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
